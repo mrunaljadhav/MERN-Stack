b97762eed320e2a5478f33f88c7114d1b87bed43
Mrunal Jadhav <mrunaljadhav131@gmail.com>
2026-01-07 22:52:31 +0530

    typing Text

--- 2. MyBrand Work Showcase and Contact Portal/script.js
+++ 2. MyBrand Work Showcase and Contact Portal/script.js
@@ -8,3 +8,11 @@ toggleBtn.addEventListener("click",()=>{
     const isLight = document.body.classList.contains("light-theme");
  toggleBtn.textContent = isLight ? "🌙" : "🌞";
 });
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Software Engineer", "Writer", "Chess Player", "Fashion Enthusiast"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
